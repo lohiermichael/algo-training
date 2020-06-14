bad95fe7aaf025ad7a44f09ef00992b3164a0b2f
Michael Lohier <lohiermichael@gmail.com>
2020-06-14 13:48:59 +0800

    Move solution of level order to the data structure to display a tree

--- easy/4.trees/data_structure.py
+++ easy/4.trees/data_structure.py
@@ -1,4 +1,7 @@
-#  Definition for a binary t
+from typing import List
+
+
+#  Definition for a binary tree
 
 
 class TreeNode:
@@ -6,3 +9,30 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
+
+
+class Useful:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        depth_dict = self.store_by_depth(root, {}, 0)
+        return self.convert_to_list(depth_dict)
+
+    def store_by_depth(self, tree: TreeNode, depth_dict: dict, current_depth: int) -> List[List[int]]:
+        if tree:
+            self.store_by_depth(tree.left, depth_dict, current_depth+1)
+            self.add_element_in_depth(depth_dict, tree.val, current_depth)
+            self.store_by_depth(tree.right, depth_dict, current_depth+1)
+        return depth_dict
+
+    def add_element_in_depth(self, depth_dict: list, element: int, current_depth: int) -> None:
+        if not current_depth in depth_dict.keys():
+            depth_dict[current_depth] = [element]
+        else:
+            depth_dict[current_depth].append(element)
+
+    def convert_to_list(self, depth_dict: dict) -> List[List[int]]:
+        L = (max(depth_dict)+1) * [0]
+        for i in range(len(L)):
+            L[i] = depth_dict[i]
+        return L
